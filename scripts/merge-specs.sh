9f2ceb53ca7633fb5e7be083a9b2d201c309119b
Bruce W. Herr II <bherr@indiana.edu>
2024-08-29 11:41:11 -0400

    Rename merged to index

--- scripts/merge-specs.sh
+++ scripts/merge-specs.sh
@@ -4,4 +4,4 @@
 npx -y openapi-merge-cli --config ./openapi-merge-config.json
 
 # Fix server URL
-perl -i -pe 's/\/subdir\/hra\//\/subdir/g' ./cached-specs/merged.json
+perl -i -pe 's/\/subdir\/hra\//\/subdir/g' ./cached-specs/index.json
